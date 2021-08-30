0c02d397bbd3d8df94881e1b38ff0a7984838a82
i67ahmed <i67ahmed@gmail.com>
2021-08-30 15:39:41 -0400

    axios installed skyscanner api connected

---
+++ routes/api/flights.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
+  params: {query: 'Stockholm'},
+  headers: {
+    'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+    'x-rapidapi-key': 'undefined'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
